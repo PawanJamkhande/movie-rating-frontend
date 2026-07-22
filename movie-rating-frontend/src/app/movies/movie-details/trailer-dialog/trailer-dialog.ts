c231b16e8001c81e800a14d6aeabc35f07a94e81
Pawan JR. <pawanjamkhande001@gmail.com>
2026-07-22 23:45:28 +0530

    Update trailer-dialog.ts

--- movie-rating-frontend/src/app/movies/movie-details/trailer-dialog/trailer-dialog.ts
+++ movie-rating-frontend/src/app/movies/movie-details/trailer-dialog/trailer-dialog.ts
@@ -1,1 +1,47 @@
+import { Component, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
+@Component({
+  selector: 'app-trailer-dialog',
+  templateUrl: './trailer-dialog.html',
+  styleUrl: './trailer-dialog.css'
+})
+export class TrailerDialog {
+
+  trailerUrl!: SafeResourceUrl;
+
+  constructor(
+    @Inject(MAT_DIALOG_DATA) public data: { trailerUrl: string },
+    private dialogRef: MatDialogRef<TrailerDialog>,
+    private sanitizer: DomSanitizer
+  ) {
+    this.trailerUrl = this.sanitizer.bypassSecurityTrustResourceUrl(
+      this.getEmbedUrl(data.trailerUrl)
+    );
+  }
+
+  close(): void {
+    this.dialogRef.close();
+  }
+
+  private getEmbedUrl(url: string): string {
+
+    if (!url) {
+      return '';
+    }
+
+    let videoId = '';
+
+    if (url.includes('youtube.com/watch?v=')) {
+      videoId = url.split('v=')[1].split('&')[0];
+    }
+
+    else if (url.includes('youtu.be/')) {
+      videoId = url.split('youtu.be/')[1].split('?')[0];
+    }
+
+    return `https://www.youtube.com/embed/${videoId}?autoplay=1`;
+  }
+
+}
